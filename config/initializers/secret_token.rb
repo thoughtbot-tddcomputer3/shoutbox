3d2ea0e28fb2f2deeb567b9fe58afc767eba10a3
tddcomputer3 <training+tddcomputer3@thoughtbot.com>
2011-01-31 15:09:21 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shoutbox::Application.config.secret_token = '685fcd8d1667f6cc3aeeaebab1a58e40b48d4cb0062d2f8772be70a4cf93c37077c244b8bda204b0e3c1014ef2bbf38d05ee4c39b4800cfb258ab987af9516ba'
